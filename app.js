8403550f05a787975c272f41bbea46a2bf11c9e9
David Woollard <dave@darve.co.uk>
2016-11-26 16:18:34 +0000

    Second commit

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
+
 var SerialPort = require('serialport'),
 	port = new SerialPort('/dev/ttyUSB0'),
 	fs = require('fs'),
-	file = String(fs.readFileSync('./files/output.hpgl'));
+	file = String(fs.readFileSync('./files/output.hpgl')),
+	interval;
  
+file = file.split(';');
+
+function draw(v) {
+	port.write(v, function(err) {
+		console.log('err', err);
+	});
+};
+
 port.on('open', function() {
-  port.write(file, function(err) {
-    if (err) {
-      return console.log('Error on write: ', err.message);
-    }
-    console.log('message written');
-  });
+
+interval = setInterval(function() {
+	
+	if ( file.length ) {
+		var temp = file.shift();
+		draw(temp);
+	} else {
+		clearInterval(interval);
+	}
+	
+}, 300);
+
+//   port.write(file, function(err) {
+//     if (err) {
+//       return console.log('Error on write: ', err.message);
+//     }
+//     console.log('message written');
+//   });
 });
  
 // open errors will be emitted as an error event 
